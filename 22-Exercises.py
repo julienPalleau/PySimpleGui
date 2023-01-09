32a02b8b8c4f7dbdfde872d34b213c60c6056f7a
julienPalleau <jpalleau@gmail.com>
2023-01-09 12:04:54 +0100

    22-Exercises

---
+++ 22-Exercises.py
@@ -0,0 +1,56 @@
+"""
+Lesson 22
+Exercise 1
+
+Use an output element to print the numbers 2 to 500
+Print the prime numbers as white on red background
+Print all the other numbers normally with no color change than the default
+Instructions
+
+    Use a Multiline element as an output element
+        In your layout, set parameter so that no values are returned for the element when window is read
+        Make the size of your Multliline be 10 chars wide by 25 rows high
+    Use the Multiline Element's print method for the output
+    If you can't figure out how to determine a prime number, check the hints
+    Use a Button (e.g. named "Start") to start the printing
+    At the top of the window show the text "Prime numbers 2 to 500". Use the same text for the Window's title
+    Remember to always check to see if the window has been closed after you read it.
+import PySimpleGUI as sg
+
+layout = [  ]
+
+window = sg.Window('', layout)
+
+while True:
+    event, values = window.read()
+
+window.close()
+"""
+import PySimpleGUI as sg
+
+import PySimpleGUI as sg
+
+layout = [  [sg.Text('Prime Numbers 2 to 500')],
+            [sg.Multiline(size=(10, 25), write_only=True, key='-MLINE-')],
+            [sg.Button('Start')]  ]
+
+window = sg.Window('Prime Numbers 2 to 500', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'Start':
+        for num in range(2, 501):
+            prime=True
+            for i in range(2, num):
+                if (num % i) == 0:
+                    prime = False
+                    break
+            if prime:
+                window['-MLINE-'].print(num, colors='white on red')
+            else:
+                window['-MLINE-'].print(num)
+
+window.close()
+
--- 22-TheOfficialPySimpleGUICourse_MultilineOutput.py
+++ 22-TheOfficialPySimpleGUICourse_MultilineOutput.py
@@ -13,15 +13,97 @@ import PySimpleGUI as sg
 
 sg.set_options(font='Default 18')
 
-example_number = 1
+example_number = 3
 
 
 # ================================== 1 - MULTILINE update & "print" EXAMPLE ============================================
-def main_mline_update():
-    mkey = '-MLINE-'
+# def main_mline_update():
+#     mkey = '-MLINE-'+sg.WRITE_ONLY_KEY
+#
+#     layout = [[sg.T('Multiline Output Using .update')],
+#               [sg.Multiline(size=(40, 15), key=mkey, disabled=True, autoscroll=True),
+#               sg.B('Go'), sg.B('Erase'), sg.B('Reroute'), sg.B('Restore'), sg.B('Exit')]]
+#
+#     window = sg.Window('Multiline Output', layout, keep_on_top=True)
+#
+#     mline = window[mkey]  # type: sg.Multiline
+#
+#     while True:  # Event Loop
+#         event, values = window.read()
+#         print(event, values)
+#         if event == sg.WIN_CLOSED or event == 'Exit':
+#             break
+#
+#         if event == 'Erase':
+#             mline.update('')
+#             continue
+#         elif event == 'Reroute':
+#             mline.reroute_stdout_to_here()
+#         elif event == 'Resotre':
+#             mline.restore_stdout()
+#
+#         mline.update(f'event = {event} ', append=True)
+#         mline.update(f'values = {values}\n', append=True)
+#     window.close()
+#
+#
+# if __name__ == "__main__":
+#     main_mline_update()
 
-    layout = [[sg.T('Multiline Output Using .update')],
-              [sg.Multiline(size=(40, 15), key=mkey)],
-              [sg.B('Go'), sg.B('Erase'), sg.B('Reroute'), sg.B('Restore'), sg.B('Exit')]]
-    
 
+# ================================== 2 - MULTILINE print EXAMPLE =======================================================
+# def main_mline_print():
+#     mkey = '-MLINE-'
+#     layout = [[sg.T('Multiline Output Using Multiline.print()')],
+#               [sg.Multiline(size=(40, 15), key=mkey, write_only=True, disabled=True, autoscroll=True),
+#                sg.B('Go'), sg.B('Right Justified'), sg.B('Exit')]]
+#
+#     window = sg.Window('Multiline.print()', layout, keep_on_top=True)
+#
+#     mline = window[mkey]  # type: sg.Multiline
+#     while True:  # Event Loop
+#         event, values = window.read()
+#         print(event, values)
+#         if event == sg.WIN_CLOSED or event == 'Exit':
+#             break
+#
+#         if event == 'Go':
+#             mline.print(f'event = {event}', text_color='red')
+#             mline.print(f'values = {values}', background_color='yellow')
+#         elif event == 'Right Justified':
+#             mline.print(f'event = {event}', t='red', justification='r')
+#             mline.print(f'values = {values}', t='green', justification='r')
+#     window.close()
+#
+#
+# if __name__ == "__main__":
+#     main_mline_print()
+
+# ================================== 3 - cprint EXAMPLE ================================================================
+def main_cprint():
+    # ----------------------------- Layout & Window
+    layout = [[sg.Multiline(size=(40, 20), k='-MLINE-', border_width=True, reroute_stdout=True, echo_stdout_stderr=True,
+                            write_only=True, autoscroll=True), sg.B('print'), sg.B('cprint')], ]
+
+    window = sg.Window('Multiline cprint', layout, keep_on_top=True)
+
+    sg.cprint_set_output_destination(window, '-MLINE-')
+
+    # ----------------------------- Event Loop
+    while True:
+        event, values = window.read()
+        # print(event, values)
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'print':
+            print(f'Event = {event} values = {values}')
+        elif event == 'cprint':
+            sg.cprint(f'Event = {event} values = {values}', c="white on red")  # c stands for color and there are 2
+            # syntax possible: c="white on red" or c = ('white', 'red')
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main_cprint()
