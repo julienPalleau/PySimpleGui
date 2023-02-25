5816eb1d3ca690c081b0a6674106420b05eb8a86
julienPalleau <jpalleau@gmail.com>
2023-02-25 09:21:10 +0100

    Fixe bug: bad indentation for retun table which was resulting in sort column not working

--- 46-TheOfficialPySimpleGUICourse_Table.py
+++ 46-TheOfficialPySimpleGUICourse_Table.py
@@ -39,12 +39,13 @@ def sort_table(table, cols):
         table: a list of lists (or tuple of tuples) where each inner list represents a row
         cols: a list (or tuple) specifying the column numbers to sort by e.g. (1, 0) would sort by column 1, then by
         column 0"""
+    print(f"Debug cols: {cols}")
     for col in reversed(cols):
         try:
             table = sorted(table, key=operator.itemgetter(col))
         except Exception as e:
-            sg.popup_error('Error in sort_table', 'Exception in sort_table, e')
-        return table
+            sg.popup_error('Error in sort_table', 'Exception in sort_table', e)
+    return table
 
 
 # ========================== 1 - Example ==============================================================================
